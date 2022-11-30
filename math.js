68bc704885692dc7bd780aabe584bd971a9355a2
Morning <morning@mstars-lab3-08s-iMac.local>
2022-11-30 12:31:23 +0800

    js ognoo hewleh

---
+++ math.js
@@ -0,0 +1,11 @@
+//Task random
+console.log(Math.random()*100+1);
+console.log(Math.random()*51 + 200) 
+
+
+//Task-general-triangle
+
+let a =33;
+let b =37;
+
+console.log(Math.sqrt(a*b));
---
+++ new.js
@@ -0,0 +1,38 @@
+// if (number.length = 8)
+//     if(a >= 86000000 && a<= 89999999){
+//         console.log(a.substr(0, 8), a.substring(2)) ;
+//     }else if (b >= 97000000 && b <= 98999999) {
+//         console.log(b);
+//     }else if (c >= 90000000 && c<= 93999999){
+//         console.log(c);
+//     }else if(q >= 94000000 && q <= 99000000){
+//         console.log(q);
+//     }else{
+//         console.log("buruu dugaar bn");
+//     }
+
+
+// let number = prompt("Ta ali operatoriin hereglegch we?");
+// let a = "unitel";
+// let b = "gmobile";
+// let c = "skytel";
+// let q = "mobicom";
+// if(number.substring(0,1) == 8){
+//     if (number.substring(1,2) == 6){
+//         console.log(a);
+//     }if (number.substring(2,3) >= 0){
+//         console.log(a);
+//     }if (number.substring(3,4) >= 0){
+//         console.log(a);
+//     }if(number.substring(4,5) >=0) {
+//         console.log(a);
+//     }if(number.substring(5,6) >=0){
+//         console.log(a);
+//     }if(number.substring(6,7) >=0) {
+//         console.log(a);
+//     }if(number.substring(7,8) >=0){
+//         console.log(a);
+//     }else{
+//         console.log("buruu dugaar bn");
+//     }
+// }
